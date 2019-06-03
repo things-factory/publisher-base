d7d312de199120b97a735991fb771b086c416b5e
horwengliang <horwengliang@gmail.com>
2019-06-03 18:47:46 +0900

    add Faye in Index

--- server/index.ts
+++ server/index.ts
@@ -3,3 +3,12 @@ export * from './migrations'
 export * from './graphql'
 // export * from './routes'
 // export * from './middlewares'
+
+const http = require('http'),
+  faye = require('faye')
+
+const server = http.createServer(),
+  bayeux = new faye.NodeAdapter({ mount: '/publisher', timeout: 30 })
+
+bayeux.attach(server)
+server.listen(8000)
